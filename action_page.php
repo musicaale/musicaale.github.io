c5c090496ebcae48b24670c1a13f27c641e26b6c
EstherAL2508 <99769631+EstherAL2508@users.noreply.github.com>
2022-03-22 17:11:22 -0600

    Create action_page.php

--- action_page.php
+++ action_page.php
@@ -1,8 +1,10 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Bienvenid@ <?php echo $_POST["name"]; ?><br>
+Tu correo electronico es: <?php echo $_POST["correo"]; ?>
+<?php echo $_POST["Fecha"]; ?>
+<?php echo $_POST["Producto"]; ?>
 
 </body>
 </html>
